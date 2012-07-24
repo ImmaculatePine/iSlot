7c8070594153ee6f94977ad208d70a867e04e696
Alexander Borovykh <immaculate.pine@gmail.com>
2012-07-24 21:08:24 +0400

    Initial Commit

---
+++ iSlot/IntroLayer.h
@@ -0,0 +1,21 @@
+//
+//  IntroLayer.h
+//  iSlot
+//
+//  Created by Александр Боровых on 24.07.12.
+//  Copyright __MyCompanyName__ 2012. All rights reserved.
+//
+
+
+// When you import this file, you import all the cocos2d classes
+#import "cocos2d.h"
+
+// HelloWorldLayer
+@interface IntroLayer : CCLayer
+{
+}
+
+// returns a CCScene that contains the HelloWorldLayer as the only child
++(CCScene *) scene;
+
+@end
